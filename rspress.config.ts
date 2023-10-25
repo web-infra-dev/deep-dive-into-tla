682e1b3e53450ed1739a04895e00ec68530e4013
chenhaoli <chenhaoli@bytedance.com>
2023-10-26 02:12:49 +0800

    feat: using `rspress-plugin-load-readme`

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
-import { addSomePages } from './rspress-plugin-add-some-pages'
+import { loadReadme } from 'rspress-plugin-load-readme';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -12,6 +12,9 @@ export default defineConfig({
     ],
   },
   plugins: [
-    addSomePages()
+    loadReadme({
+      repo: 'ulivz/deep-dive-into-tla',
+      route: '/post'
+    })
   ]
 });
